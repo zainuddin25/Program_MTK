77411e212b915628ae6ed783ddacc61de862f55c
Reza Aditya Pratama_31 <rezaaditya9132@gmail.com>
2020-09-18 11:44:06 +0700

    keliling rusak

---
+++ keliling.php
@@ -0,0 +1,93 @@
+<?php
+echo "sistem operasi aritmatika\n";
+// echo "keliling Lingkaran \n";
+echo "=========================\n";
+
+// Reza Aditya Pratama
+
+// $keluar=true;
+
+// while($keluar){
+//     $angka_satu=readline("masukan diameter lingkaran\t: ");
+    
+//     //validasi input angka pertama
+//     if(!preg_match("/^[0-9]*$/",$angka_satu)){//jika $angka_satu bukan angka
+//         echo "maaf input hanya bisa angka !!!\n";
+//     continue;
+//     }
+
+//     echo "keliling dari lingkaran yang berdiameter $angka_satu adalah: ".($angka_satu*(22/7))."\n";
+
+//     $ulangi=readline("ulangi lagi(yes/no): ");
+//     if($ulangi!=="yes" || $ulangi=="no" ){
+//         $keluar=false;
+//     }
+// }
+
+$data_menu=["penjumlahan","pengurangan","perkalian","pembagian","keliling lingkaran"];
+
+$berhenti=true;
+while($berhenti)
+{
+    $nomor=1 ;
+    foreach($data_menu as $data)
+    {
+        echo $nomor.$data."\n";
+        $nomor++;
+    }
+
+    $pilih_menu=readline("Pili menu :");
+
+    if($pilih_menu==1)
+    {
+        // Fitur Keliling Lingkaran
+        echo "penjumlahan\n";
+        include('program_matematika.php');
+        
+    }elseif($pilih_menu==2)
+    {
+        // Fitur Pengurangan
+        echo "pengurangan\n";
+        include('pengurangan.php');
+    }elseif($pilih_menu==3)
+    {
+        echo "perkalian\n";
+        //perkalian
+        include('perkalian.php');
+    }elseif($pilih_menu==4){
+        echo "pembagian\n";
+        //pembagian
+        include('pembagian.php');
+    }elseif($pilih_menu==5){
+        echo "keliling\n";
+        //keliling
+        $keluar=true;
+
+            while($keluar){
+                $angka_satu=readline("masukan angka pertama\t: ");
+                
+                //validasi input angka pertama
+                if(!preg_match("/^[0-9]*$/",$angka_satu)){//jika $angka_satu bukan angka
+                    echo "maaf input hanya bisa angka !!!\n";
+                continue;
+                }
+
+                echo "keliling dari lingkaran yang berdiameter $angka_satu adalah: ".($angka_satu*(22/7))."\n";
+
+                $ulangi=readline("ulangi lagi(yes/no): ");
+                if($ulangi!=="yes" || $ulangi=="no" ){
+                    $keluar=false;
+                }
+            }
+    }else{
+        echo "anda tidak memilih menu";
+    }
+
+    // $ulangi=readline("ulangi lagi");
+    // if($ulangi=="no")
+    // {
+    //     $berhenti=false;
+    // }
+
+
+}
